46dbb9d4da93e0de3b921fbf3d72156e1b53e0ca
Merus23 <mateus.satira@gmail.com>
2024-01-16 12:40:54 -0300

    feat: add the App syle

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteúdo</h1>
+      <main className={styles.main}>
+        <h1>asd</h1>
+      </main>
       <Footer />
     </div>
   );
