82054c6359ce07be9e0ff8913f8aa3be5476e3de
Merus23 <mateus.satira@gmail.com>
2024-01-23 10:22:10 -0300

    feat: finish TaskList

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,33 @@
 import {} from "react";
 
-type Props = {};
+import { ITask } from "../interfaces/Task";
 
-const TaskList = (props: Props) => {
+import styles from "./TaskList.module.css";
+
+interface Props {
+  taskList: ITask[];
+}
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <h1>Lista de tarefas</h1>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não tem tarefas cadastradas!</p>
+      )}
+    </>
   );
 };
 
